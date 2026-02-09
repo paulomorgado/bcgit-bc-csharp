cc9ee80e71d36723217247702dcea3bf3d3733c3
Paulo Morgado <470455+paulomorgado@users.noreply.github.com>
2026-02-09 19:31:30 +0000

    Add BouncyCastle.Crypto.Benchmarks project with AES tests
    Added a new benchmark project using BenchmarkDotNet to measure AES engine initialization and block processing performance. Updated the solution file to include the new project and build configurations. Implemented AesEngineBenchmarks and a Program entry point to run all benchmarks.

---
+++ crypto/benchmark/BouncyCastle.Crypto.Benchmarks/AesEngineBenchmarks.cs
@@ -0,0 +1,44 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Jobs;
+using Org.BouncyCastle.Crypto.Engines;
+using Org.BouncyCastle.Crypto.Parameters;
+
+namespace BouncyCastle.Crypto.Benchmarks
+{
+    [MemoryDiagnoser]
+    [SimpleJob(RuntimeMoniker.Net10_0)]
+    [SimpleJob(RuntimeMoniker.Net481)]
+    [HideColumns(Column.Error, Column.StdDev, Column.Mean)]
+    public class AesEngineBenchmarks
+    {
+        private KeyParameter key;
+        private AesEngine aesForInit;
+        private AesEngine aesForEncrypt;
+        private byte[] output = new byte[256];
+        private byte[] iv = new byte[] { 0x59, 0x5B, 0x69, 0x9B, 0xBD, 0x3B, 0xC0, 0xDF, 0x26, 0x06, 0x20, 0x93, 0xC1, 0xAD, 0x8F, 0x73 };
+
+        [IterationSetup]
+        public void IterationSetup()
+        {
+            key = new KeyParameter(new byte[] { 0x23, 0x48, 0x29, 0x00, 0x84, 0x67, 0xbe, 0x18, 0x6c, 0x3d, 0xe1, 0x4a, 0xae, 0x72, 0xd6, 0x2c });
+
+            aesForInit = new AesEngine();
+
+            aesForEncrypt = new AesEngine();
+            aesForEncrypt.Init(true, key);
+        }
+
+        [Benchmark]
+        public void Init()
+        {
+            aesForInit.Init(true, key);
+        }
+
+        [Benchmark]
+        public void ProcessBlock()
+        {
+            aesForEncrypt.ProcessBlock(iv, 0, output, 16);
+        }
+    }
+}
---
+++ crypto/benchmark/BouncyCastle.Crypto.Benchmarks/Program.cs
@@ -0,0 +1,14 @@
+﻿using BenchmarkDotNet.Running;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+
+
+namespace BouncyCastle.Crypto.Benchmarks
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+        }
+    }
+}
